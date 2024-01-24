ee3147e7019e27687afe0cfad26ac70abc90fde1
xamjjjik <averinkd@mail.ru>
2024-01-24 14:01:15 +0300

    almost complete calculations

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
+let today = new Date();
+const button = document.getElementById("button");
+button.onclick = getBirthday;
+
 function getBirthday() {
   let inputDay = document.getElementById("inputDay").value;
   let inputMonth = document.getElementById("inputMonth").value;
   let inputYear = document.getElementById("inputYear").value;
-  errorInputAlert(inputDay);
-  errorInputAlert(inputMonth);
-  errorInputAlert(inputYear);
-  console.log(inputDay, inputMonth, inputYear);
-  console.log(calculateTime(inputDay, inputMonth, inputYear));
-}
-
-const button = document.getElementById("button");
-button.onclick = getBirthday;
-
-function errorInputAlert(element) {
-  if (element !== Number) {
-    switch (element) {
-      case inputDay:
-        document.getElementById("inputDay").className += " error";
-        break;
-      case inputMonth:
-        document.getElementById("inputMonth").className += " error";
-        break;
-      case inputYear:
-        document.getElementById("inputYear").className += " error";
-        break;
-    }
-  }
+  console.log(
+    (today - calculateTime(inputDay, inputMonth, inputYear)) / 1000 / 3600 / 24
+  );
 }
 
 let yearsResult = document.getElementById("yearsResult").outerHTML;
 let monthsResult = document.getElementById("monthsResult").outerHTML;
 let daysResult = document.getElementById("daysResult").outerHTML;
 
-// console.log(yearsResult, monthsResult, daysResult);
-
 function calculateTime(day, month, year) {
   let d = new Date(year, month - 1, day);
-  return d && d.getMonth() + 1 == month && d.getDate() == Number(day);
+  // day === Number && month === Number && year === Number
+  // d && d.getMonth() + 1 == month && d.getDate() == Number(day)
+  if (d && d.getMonth() + 1 == month && d.getDate() == Number(day)) {
+    return d;
+  } else {
+    document.getElementById("inputDay").className += " error";
+
+    document.getElementById("inputMonth").className += " error";
+
+    document.getElementById("inputYear").className += " error";
+  }
 }
