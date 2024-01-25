4f79383a2a11f2e298335062ca069b9596f2a559
xamjjjik <averinkd@mail.ru>
2024-01-25 15:08:24 +0300

    made reset to default when error input

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function getBirthday() {
   let inputYear = document.getElementById("inputYear").value;
   let toDays = 3600000 * 24 * 365;
   let inputDate = calculateTime(inputDay, inputMonth, inputYear);
-  console.log(inputDate);
   if (inputDate !== NaN && inputDate !== undefined) {
     let yearsResult = (today - inputDate) / toDays;
 
@@ -17,13 +16,23 @@ function getBirthday() {
     let newDays = Math.floor(daysResult);
     let newMonths = Math.floor(monthsResult);
     let newYears = Math.floor(yearsResult);
-    setNewInterval(newYears, newMonths, newDays);
-    document.getElementById("inputDay").classList.remove("error");
-    document.getElementById("inputMonth").classList.remove("error");
-    document.getElementById("inputYear").classList.remove("error");
-    document.getElementById("error-text").innerHTML = "";
+    if (newDays < 0 || newMonths < 0 || newYears < 0) {
+      returnToDefault();
+      document.getElementById(
+        "error-text"
+      ).innerHTML = `<div class="error-fields">Must be in the past</div> <div class="error-fields">Must be in the past</div> <div class="error-fields">Must be in the past</div>`;
+    } else {
+      setNewInterval(newYears, newMonths, newDays);
+      document.getElementById("inputDay").classList.remove("error");
+      document.getElementById("inputMonth").classList.remove("error");
+      document.getElementById("inputYear").classList.remove("error");
+      document.getElementById("error-text").innerHTML = "";
+    }
   } else {
-    document.getElementById("error-text").innerHTML = "Must input a valid date";
+    returnToDefault();
+    document.getElementById(
+      "error-text"
+    ).innerHTML = `<div class="error-fields">Enter some number</div> <div class="error-fields">Enter some number</div> <div class="error-fields">Enter some number</div>`;
   }
 }
 function setNewInterval(years, months, days) {
@@ -37,14 +46,10 @@ function setNewInterval(years, months, days) {
     "daysResult"
   ).innerHTML = `${days} <span class="color-text">days</span>`;
 }
-// let yearsResult = document.getElementById("yearsResult").outerHTML;
-// let monthsResult = document.getElementById("monthsResult").outerHTML;
-// let daysResult = document.getElementById("daysResult").outerHTML;
 
 function calculateTime(day, month, year) {
   let d = new Date(year, month - 1, day);
-  // day === Number && month === Number && year === Number
-  // d && d.getMonth() + 1 == month && d.getDate() == Number(day)
+
   if (d && d.getMonth() + 1 == month && d.getDate() == Number(day)) {
     return d;
   } else {
@@ -54,4 +59,20 @@ function calculateTime(day, month, year) {
 
     document.getElementById("inputYear").className += " error";
   }
+}
+
+function returnToDefault() {
+  let defaultHTML = [
+    (document.getElementById(
+      "yearsResult"
+    ).innerHTML = `-- <span class="color-text">years</span>`),
+    (document.getElementById(
+      "monthsResult"
+    ).innerHTML = `-- <span class="color-text">months</span>`),
+    (document.getElementById(
+      "daysResult"
+    ).innerHTML = `-- <span class="color-text">days</span>`),
+  ];
+
+  return defaultHTML[0], defaultHTML[1], defaultHTML[2];
 }
